7bdf867d1ddcbca9208e992c00bac35cea25d3a5
Fernando Fabricio dos Santos <ffsantos@uolinc.com>
2016-01-27 11:36:21 -0200

    changing the testes to unittest lib

--- L5P2.py
+++ L5P2.py
@@ -1,3 +1,5 @@
+import unittest
+
 def recurPower(base, exp):
     '''
     base: int or float.
@@ -10,8 +12,23 @@ def recurPower(base, exp):
     else:
         return base * recurPower(base, exp - 1)
 
+class TestRecurPower(unittest.TestCase):
 
-print recurPower(3, 0) == 1.00
-print recurPower(3, 3) == 27
-print recurPower(5, 2) == 25
-print recurPower(11, 2) == 121
+    def test_three_power_zero(self):
+        self.assertEqual(round(recurPower(3, 0), 4), 1.0000)
+
+    def test_three_power_three(self):
+        self.assertEqual(round(recurPower(3, 3), 4), 27.0000)
+
+    def test_five_power_two(self):
+        self.assertEqual(round(recurPower(5, 2),4), 25.0000)
+
+    def test_eleven_power_two(self):
+        self.assertEqual(round(recurPower(11, 2),4), 121.0000)
+
+    def test_negative_power_zero(self):
+        self.assertEqual(round(recurPower(-7.62, 0),4), 1.0000)
+
+
+if __name__ == '__main__':
+    unittest.main()
