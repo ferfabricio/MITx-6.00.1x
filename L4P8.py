8b9dd20fc0662f5eb286b0101ca71dde3bac4b72
Fernando Fabricio dos Santos <ffsantos@uolinc.com>
2016-01-27 12:39:44 -0200

    Add testes with unittest lib

--- L4P8.py
+++ L4P8.py
@@ -1,3 +1,5 @@
+import unittest
+
 def square(x):
     '''
     x: int or float.
@@ -7,5 +9,17 @@ def square(x):
 def fourthPower(x):
     return square(square(x))
 
-print fourthPower(3)
-print fourthPower(7)
+class TestFourthPower(unittest.TestCase):
+
+    def test_with_three(self):
+        self.assertEqual(fourthPower(3), 81)
+
+    def test_with_seven(self):
+        self.assertEqual(fourthPower(7), 2401)
+
+    def test_with_float(self):
+        self.assertEqual(round(fourthPower(1.1),4), 1.4641)
+
+
+if __name__ == '__main__':
+    unittest.main()
